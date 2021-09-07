9fcbe1049d97942695e5083078533365cbe1f02b
Soanvig <soanvig@gmail.com>
2021-09-07 21:55:16 +0200

    Parse command name from argument

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,24 @@ module Main where
 
   unpackCommands :: [Either ParseError Command] -> Either ParseError [Command]
   unpackCommands = sequence
-  
+
+  userCommand :: [String] -> String
+  userCommand [] = undefined
+  userCommand (x : xs) = x
+
+  matchCommand :: String -> [Command] -> Command
+  matchCommand cmd [] = undefined
+  matchCommand cmd (command@(Command cmdName _ _) : xs) | cmd == cmdName = command
+  matchCommand cmd (x : xs) = matchCommand cmd xs
+
   main :: IO ()
   main = do
     contents <- readFileText "test.cli"
     args <- getArgs
+
     let commands = map (runParser . unpack) (lines contents)
 
     case unpackCommands commands of
       Left err -> print err
-      Right commands -> executeCommandList commands
+      Right commands -> executeCommand $ matchCommand (userCommand args) commands
       
