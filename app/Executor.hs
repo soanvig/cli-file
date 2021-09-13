d2e7ef531f75de0f7e5adf3a787315189a51c8e7
Soanvig <soanvig@gmail.com>
2021-09-13 09:28:39 +0200

    replace arguments

--- app/Executor.hs
+++ app/Executor.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Executor where
   
   import System.Process (shell, createProcess, CreateProcess)
-  import Symbol
+  import Data.Text (replace, pack, unpack)
+  import Symbol ( Argument(..), Command(..), UserCommand(..) )
 
-  replaceArgs :: Command -> UserCommand -> Command
-  replaceArgs _ _ = Command "" [] ""
+  type Param = (String, String) -- param name, param value
+
+  matchArgs :: [Argument] -> [String] -> [Param]
+  matchArgs [] [] = []
+  matchArgs [] (userArg : userArgRest) = undefined
+  matchArgs ((ArgumentRequired _) : argRest) [] = undefined
+  matchArgs ((ArgumentOptional name value) : argRest) [] = (name, value) : matchArgs argRest []
+  matchArgs ((ArgumentRequired name) : argRest) (value : userArgRest) = (name, value) : matchArgs argRest userArgRest
+  matchArgs ((ArgumentOptional name _) : argRest) (value : userArgRest) = (name, value) : matchArgs argRest userArgRest
+
+  replaceParams :: String -> [Param] -> String
+  replaceParams command [] = command
+  replaceParams command (param : paramRest) = replaceParams (unpack replacedCommand) paramRest
+    where replacedCommand = replace (pack $ '$' : fst param) (pack $ snd param) (pack command)
 
   executeCommand :: Command -> UserCommand -> IO ()
   executeCommand (Command _ commandArgs command) (UserCommand _ userCommandArgs) = do
-    _ <- createProcess $ shell command
+    _ <- createProcess $ shell (replaceParams command $ matchArgs commandArgs userCommandArgs)
     return ()
