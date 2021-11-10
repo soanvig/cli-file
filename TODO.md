200deeb699b246dc4f840737def9ea077a83d50d
Soanvig <soanvig@gmail.com>
2021-11-11 00:16:42 +0100

    feat: add --version/-v and --help/-h switches

--- TODO.md
+++ TODO.md
@@ -3,6 +3,8 @@
 - [ ] Calling command without parameter should show interactive selection of command (or optionally with --interactive flag?)
 - [ ] Add CI with build and release
 - [ ] README documentation
+- [ ] Add support for longer than one-word argument default values and values passed as input
+- [x] Add --version and --help commands
 - [x] Add CI with tests
 - [x] Support for empty lines in command file
 - [x] Add support for comments
--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
   import Lib (executeCommand, buildCommand)
+  import Data.Text (unpack)
+
+  help = unpack $ unlines [
+    "cli-file - tool for easy and programming language-agnostic command-line interfaces for executing commands",
+    "It offers domain-specific language: configuration file containing `myEcho :: value = World -> echo Hello $value`",
+    "can be executed using `cli-file myEcho` resulting in 'Hello World' or `cli-file myEcho John` resulting in 'Hello John'`",
+    "",
+    "Alpha version: API is subjected to change",
+    "For more details see: https://github.com/soanvig/cli-file"
+    ]
+  version = "0.0.1"
 
   main :: IO ()
   main = do
     contents <- readFileText "commands.cli"
     args <- getArgs
 
-    let command = buildCommand contents args
-
-    case command of
-      Left err -> putStrLn err
-      Right shellCommand -> executeCommand shellCommand
+    case viaNonEmpty head args of
+      Just "--help" -> putStrLn help
+      Just "-h" -> putStrLn help
+      Just "--version" -> putStrLn version
+      Just "-v" -> putStrLn version
+      _ -> case buildCommand contents args of
+        Left err -> putStrLn err
+        Right shellCommand -> executeCommand shellCommand
       
       
